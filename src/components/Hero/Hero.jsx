b5f795ea8ecf4426fbcdabb127a563d535f2a833
Viktor <vitygocanal@gmail.com>
2025-09-10 08:05:04 +0200

    some changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,17 @@
 import "./Hero.css"
+import { IoSearch } from "react-icons/io5";
 
 
 export default function Hero(){
     return(
         <section className="hero-section">
+            <h1 className="hero-section__title">Make Your Interior More Mininimalistic & Modern</h1>
+            <p className="hero-section__text">Turn your room with panto into a lot more minimalist and modern with ease and speed</p>
+            <div className="hero-section__search-container">
+                <input className="search-container__input" placeholder='Search furniture'/>
+                <button className="search-container__btn"><IoSearch color="white" size='18px'/>
+                </button>
+            </div>
 
 
         </section>
