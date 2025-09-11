98ea720b9b9d3c064ed845fba025a28e87e1908d
Viktor <vitygocanal@gmail.com>
2025-09-11 09:01:20 +0200

    new section

--- src/components/BestProducts/BestProducts.jsx
+++ src/components/BestProducts/BestProducts.jsx
@@ -7,19 +7,26 @@ export default function BestProducts(){
 
 
     const chair = [
-        {categoryName: 'chair', productName: 'Sakarias Armchair', productPrice: 392},
-        {categoryName: 'chair', productName: 'Baltsar Chair', productPrice: 299},
-        {categoryName: 'chair', productName: 'Anjay Chair', productPrice: 519},
-        {categoryName: 'chair', productName: 'Nyantuy Chair', productPrice: 921},
+        {key: 1, categoryName: 'chair', productName: 'Sakarias Armchair', productPrice: 392},
+        {key: 2, categoryName: 'chair', productName: 'Baltsar Chair', productPrice: 299},
+        {key: 3, categoryName: 'chair', productName: 'Anjay Chair', productPrice: 519},
+        {key: 4, categoryName: 'chair', productName: 'Nyantuy Chair', productPrice: 921},
     ]
 
 
     const categories = [
-        {categoryName: 'chair'},
-        {categoryName: 'beds'},
-        {categoryName: 'sofa'},
-        {categoryName: 'lamp'},
+        {categoryName: 'chair', categoryArray: chair},
+        {categoryName: 'beds', categoryArray: chair},
+        {categoryName: 'sofa', categoryArray: chair},
+        {categoryName: 'lamp', categoryArray: chair},
     ]
+
+    // useEffect(() => {
+
+
+
+
+    // },[activeCategory])
 
 
     function handleCategory(categoryName){
@@ -32,25 +39,32 @@ export default function BestProducts(){
             <h2 className='best-products-section__title'>Best Selling Product</h2>
             <div className="best-products-section__navigation">
                 {categories.map(category => <button className={`navigation__category ${activeCategory == category.categoryName ? 'active' : ''}`} onClick={() => handleCategory(category.categoryName)}>{category.categoryName}</button>)}
+               
             </div>
+            {categories.map(
+              category => {if (category.categoryName === activeCategory
+              ){
+                    category.categoryArray.map(
+                        item => <ProductCard key={item.key} categoryArray={item}/>
+                    )}})};
 
 
         </section>
     )
 }
 
-export function ProductCard() {
+export function ProductCard({categoryArray}) {
     return(
         <div className="prdoduct-card-container">
             <div className="product-card__img-container">
             <img alt='product' src='#' className='product-card__img'/>
             </div>
             <div className="product-card__body">
-                <p className='body__product-category'></p>
-                <h3 className='body__product-name'></h3>
+                <p className='body__product-category'>{categoryArray.categoryName}</p>
+                <h3 className='body__product-name'>{categoryArray.productName}</h3>
                 <div className="body__product-rewiew"></div>
                 <div className="body__footer">
-                    <p className='footer__product-price'></p>
+                    <p className='footer__product-price'>{categoryArray.productPrice}</p>
                     <button className='footer__add-to-cart-btn'>+</button>
                 </div>
             </div>
