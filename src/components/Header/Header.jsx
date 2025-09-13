6259330246bf0aabd9839fe210b9c325a5b98752
Viktor <vitygocanal@gmail.com>
2025-09-13 14:09:33 +0200

    responsivnes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import './Header.css'
 import { PiHandbagFill } from "react-icons/pi";
-
+import { GiHamburgerMenu } from "react-icons/gi";
 
 
 export default function Header() {
@@ -8,16 +8,29 @@ export default function Header() {
         <header>
             <div className="navbar">
                 <div className="navbar__logo">Logo</div>
-                <ul className="navbar__menu">
-                    <li className="menu__elemnt menu__elemnt--dropdown">Furniture</li>
-                    <li className="menu__elemnt">Shop</li>
-                    <li className="menu__elemnt">About Us</li>
-                    <li className="menu__elemnt">Contact</li>
-                </ul>
-                <div className="navbar__cart">
-                <button><PiHandbagFill color="white" size="30px"/></button>
+                <div className="nabar__left-side">
+                        
+                    <div className="navbar__cart">
+                        <button><PiHandbagFill color="white" size="30px"/></button>
+                    </div>
+
+                    <div className="navbar__drop-down-menu">
+                        <GiHamburgerMenu color="white" size="30px"/>
+                    </div>
+
                 </div>
+          
+                
             </div>
         </header>
     )
 }
+
+
+   {/* <ul className="navbar__menu">
+
+                    <li className="menu__elemnt menu__elemnt--dropdown">Furniture</li>
+                    <li className="menu__elemnt">Shop</li>
+                    <li className="menu__elemnt">About Us</li>
+                    <li className="menu__elemnt">Contact</li>
+                </ul> */}
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import { IoSearch } from "react-icons/io5";
 export default function Hero(){
     return(
         <section className="hero-section">
-            <h1 className="hero-section__title">Make Your Interior More Mininimalistic & Modern</h1>
+            <h1 className="hero-section__title">Make  Your <br/> Interior More Mininimalistic & Modern</h1>
             <p className="hero-section__text">Turn your room with panto into a lot more minimalist and modern with ease and speed</p>
             <div className="hero-section__search-container">
                 <input className="search-container__input" placeholder='Search furniture'/>
