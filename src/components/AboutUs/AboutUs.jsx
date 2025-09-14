29531df55a661f88bd47780dc83f943db916bbc9
Viktor <vitygocanal@gmail.com>
2025-09-14 08:54:31 +0200

    new section

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -3,6 +3,24 @@ import './AboutUs.css'
 
 export default function AboutUs(){
     return(
-        <section className="aboutus-section"></section>
+        <section className="aboutus-section">
+            <div className="aboutus__about about">
+                <div className="aboutus__images"></div>
+                <div className="aboutus__descr"></div>
+            </div>
+            <div className="aboutus__about about">
+            <div className="aboutus__descr"></div>
+            <div className="aboutus__images"></div>
+            </div>
+            <div className="aboutus__rewiews about">
+                <h3 className=""></h3>
+                <h3 className=""></h3>
+                <div className="rewiews">
+                    <div className="rewiew"></div>
+                    <div className="rewiew"></div>
+                    <div className="rewiew"></div>
+                </div>
+            </div>
+        </section>
     )
 }
