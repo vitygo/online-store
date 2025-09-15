4c7afc734cb5076ed1ace5234a21527b4fc85e68
Viktor <vitygocanal@gmail.com>
2025-09-15 13:31:06 +0200

    new section

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -4,14 +4,36 @@ import './AboutUs.css'
 export default function AboutUs(){
     return(
         <section className="aboutus-section">
+
             <div className="aboutus__about about">
-                <div className="aboutus__images"></div>
-                <div className="aboutus__descr"></div>
+
+                <div className="aboutus__images">
+                    <img src='./rectangle.png' className="images__img" alt=''></img>
+                </div>
+
+                <div className="aboutus__descr">
+                    <p className="descr__subject">EXPERIENCES</p>
+                    <h3 className="descr__title">We Provide You The Best Experience</h3>
+                    <p className="descr__text">You don’t have to worry about the result because all of these interiors are made by people who are professionals in their fields with an elegant and lucurious style and with premium quality materials</p>
+                    <button className="descr__btn">More info</button>
+                    
+                </div>
+
             </div>
+
             <div className="aboutus__about about">
-            <div className="aboutus__descr"></div>
-            <div className="aboutus__images"></div>
+            <div className="aboutus__descr">
+                    <p className="descr__subject">MATERIALS</p>
+                    <h3 className="descr__title">Very Serious Materials For Making Furniture</h3>
+                    <p className="descr__text">Because panto was very serious about designing furniture for our environment, using a very expensive and famous capital but at a relatively low price</p>
+                    <button className="descr__btn">More info</button>
+                    
+                </div>
+                <div className="aboutus__images">
+                    <img src='./rectangle2.png' className="images__img" alt=''></img>
+                </div>
             </div>
+
             <div className="aboutus__rewiews about">
                 <h3 className=""></h3>
                 <h3 className=""></h3>
