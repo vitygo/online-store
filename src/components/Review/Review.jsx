18eac788f8ed63d448e2fc045e13f847499d20e7
Viktor <vitygocanal@gmail.com>
2025-09-16 10:45:26 +0200

    responsive

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,5 +1,11 @@
 import './Review.css'
 import './ReviewCard.css'
+import { CiStar } from "react-icons/ci";
+import { FaStar } from "react-icons/fa";
+
+
+
+import React, {useState, useEffect} from 'react'
 
 
 export default function Review(){
@@ -11,7 +17,8 @@ export default function Review(){
           name: "Bang Upin",
           occupation: "Pedagang Asongan",
           reviewText: "Terimakasih banyak, kini ruanganku menjadi lebih mewah dan terlihat mahal",
-          rating: 4
+          bgImg: "./bg1.png",
+          productRating: 1
         },
         {
           id: 2,
@@ -19,7 +26,8 @@ export default function Review(){
           name: "Ibuk Sukijan",
           occupation: "Ibu Rumah Tangga",
           reviewText: "Makasih Panto, aku sekarang berasa tinggal di apartment karena barang-barang yang terlihat mewah",
-          rating: 4
+          bgImg: "./b2.png",
+          productRating: 1
         },
         {
           id: 3,
@@ -27,7 +35,8 @@ export default function Review(){
           name: "Mpok Ina",
           occupation: "Karyawan Swasta",
           reviewText: "Sangat terjangkau untuk kantong saya yang tidak terlalu banyak",
-          rating: 4
+          bgImg: "./b3.png",
+          productRating: 5
         }
       ];
       
@@ -41,11 +50,12 @@ export default function Review(){
                 {reviews.map(review =>  
                 <ReviewCard 
                     key={review.id} 
-                    img={review.img} 
+                    img={review.image} 
                     name={review.name} 
                     occupation={review.occupation} 
-                    rating={review.rating}
-                    reviewText={review.reviewText}/>
+                    reviewText={review.reviewText}
+                    bgImg={review.bgImg}
+                    productRating={review.productRating}/>
            
                     )}
               
@@ -57,15 +67,32 @@ export default function Review(){
 }
 
 
-export function ReviewCard({img, name, occupation, rating, reviewText}){
+export function ReviewCard({img, name, occupation, reviewText, bgImg, productRating}){
+
+    
+    
+
+
+
     return(
-        <div className="review-card">
+        <div className="review-card" style={{backgroundImage: `url(${bgImg})`}}>
             <div className="review-card__body">
                 <img src={img} alt=''className='body__img'/>
                 <p className='body__username'>{name}</p>
                 <p className='body__user-occupation'>{occupation}</p>
                 <p className='body__user-review-text'>{reviewText}</p>
-                <p>{rating}</p>
+                <div className="rating">
+
+            {Array(productRating).fill().map((_, index) => (
+                <FaStar key={`full-${index}`} color="orange" />
+            ))}
+
+            
+            {Array(5 - productRating).fill().map((_, index) => (
+                <CiStar key={`empty-${index}`} />
+            ))}
+                                
+                </div>
 
             </div>
 
